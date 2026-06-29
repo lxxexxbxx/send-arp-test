5b8263c8524e445d38e99cd2023c8650971090a6
kali <ei228555@gmail.com>
2026-06-29 15:49:27 +0900

    complete report send-arp-test

--- src/main.cpp
+++ src/main.cpp
@@ -31,19 +31,19 @@ int main(int argc, char* argv[]) {
 
 	EthArpPacket packet;
 
-	packet.eth_.dmac_ = Mac("00:00:00:00:00:00");
-	packet.eth_.smac_ = Mac("00:00:00:00:00:00");
+	packet.eth_.dmac_ = Mac("90:de:80:d5:ad:17");
+	packet.eth_.smac_ = Mac("90:de:80:3f:71:89");
 	packet.eth_.type_ = htons(EthHdr::Arp);
 
 	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
 	packet.arp_.pro_ = htons(EthHdr::Ip4);
 	packet.arp_.hln_ = Mac::Size;
 	packet.arp_.pln_ = Ip::Size;
-	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.sip_ = htonl(Ip("0.0.0.0"));
-	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.tip_ = htonl(Ip("0.0.0.0"));
+	packet.arp_.op_ = htons(ArpHdr::Reply);
+	packet.arp_.smac_ = Mac("90:de:80:3f:71:89");
+	packet.arp_.sip_ = htonl(Ip("172.20.10.1"));
+	packet.arp_.tmac_ = Mac("90:de:80:d5:ad:17");
+	packet.arp_.tip_ = htonl(Ip("172.20.10.5"));
 
 	int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 	if (res != 0) {
